4cad621fedbbf3a0a85878f0c3d0b77bee3b6bd5
Murad Akhrarov <makhrarov@istreamplanet.com>
2017-04-11 00:20:39 -0700

    Init

---
+++ DirTree/BuildDirTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirTree
+{
+    static class BuildDirTree
+    {
+        private static int depth = 0;
+        private static List<string> ExcludedDirsList = new List<string>();
+
+        internal static void InitExcludedDirsList()
+        {
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            //ExcludedDirsList.Add(@"");
+            ExcludedDirsList.Add(@"C:\Aventus\Trunk\Deployment");
+            ExcludedDirsList.Add(@"C:\Aventus\Trunk\log4mongo-net");
+            ExcludedDirsList.Add(@"C:\Aventus\Trunk\packages\");
+            ExcludedDirsList.Add(@"C:\Aventus\Trunk\packages\knockout");
+            ExcludedDirsList.Add(@"C:\Aventus\Trunk\packages\Microsoft.AspNet");
+            ExcludedDirsList.Add(@"C:\Aventus\Trunk\RunningTortoise\ThirdParty");
+            ExcludedDirsList.Add(@"C:\Aventus\Trunk\Tools");
+        }
+
+        internal static void Build(string path)
+        {
+            if (ExcludedDirsList.Any(s => s.Contains(path)))
+            {
+                return;
+            }
+
+            Console.WriteLine(path.PadLeft(depth));
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                depth++;
+                Build(dir);
+                depth--;
+            }
+
+        }
+    }
+}
---
+++ DirTree/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fclp;
+
+namespace DirTree
+{
+    class Args
+    {
+        public string Path { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = ParserArgs(args);
+            BuildDirTree.Build(path);
+        }
+
+        static string ParserArgs(string[] args)
+        {
+            var parser = new FluentCommandLineParser<Args>();
+            parser.Setup(a => a.Path)
+                .As('p', "path")
+                .Required();
+
+            var result = parser.Parse(args);
+            if (result.HasErrors)
+            {
+                throw new Exception(parser.Object.ToString());
+            }
+
+            return parser.Object.Path;
+        }
+    }
+}
